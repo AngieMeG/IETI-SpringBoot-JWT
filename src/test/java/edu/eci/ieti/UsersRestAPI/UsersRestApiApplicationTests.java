c0836d07be3097a1c7d439512e0e601fac07bb34
Angie Medina <angie.medina@mail.escuelaing.edu.co>
2022-01-28 16:01:37 -0500

    Spring initializr

---
+++ src/test/java/edu/eci/ieti/UsersRestAPI/UsersRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.UsersRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
